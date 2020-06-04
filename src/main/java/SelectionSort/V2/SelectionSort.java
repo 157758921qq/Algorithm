ba345d5a0044b5df1e278a8ec187e57944be8a4e
157758921qq <35675412+157758921qq@users.noreply.github.com>
2020-06-04 08:38:47 +0800

    1、算法：选择排序

--- src/main/java/SelectionSort/V2/SelectionSort.java
+++ src/main/java/SelectionSort/V2/SelectionSort.java
@@ -1,4 +1,4 @@
-package V2;
+package SelectionSort.V2;
 /**
  * 算法：由简单到复杂，由粗糙到精细
  * 思路：经过多次遍历，每次遍历找出最小的那个数，放在头的位置
@@ -8,12 +8,13 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {5, 3, 6, 1, 9, 0, 12, 19, 2};
 
+        //这里有个点，最后一位，直接
         for (int i = 0; i < arr.length - 1; i++) {
             int minPos = i;
             for (int j = i + 1; j < arr.length; j++) {
                 minPos = arr[j] < arr[minPos] ? j : minPos;
             }
-            System.out.println("minPos:" + minPos);
+            System.out.println("最小值的位置minPos:" + minPos);
             swap(arr, i, minPos);
             System.out.println("经过"+ i +"次循环");
             print(arr);
