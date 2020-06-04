6ab3db55c06ad1d048ea6ae23f770af8099ebeec
157758921qq <35675412+157758921qq@users.noreply.github.com>
2020-06-04 09:25:56 +0800

    1、BubbleSort

---
+++ src/main/java/BubbleSort/BubbleSort.java
@@ -0,0 +1,32 @@
+package BubbleSort;
+
+public class BubbleSort {
+
+
+    public static void sort() {
+        int[] a = {9,3,1,4,6,8,7,5,2};
+        sort(a);
+        print(a);
+    }
+
+    static void sort(int[] a){
+        for (int i=a.length-1; i>0; i--){
+            for (int j=0; j<i; j++){
+                if (a[j] > a[j+1]) swap(a, j, j+1);
+            }
+        }
+
+    }
+
+    static void swap(int[] a, int i, int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    static void print(int[] arr){
+        for (int i=0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ src/main/java/BubbleSort/DataCheck.java
@@ -0,0 +1,46 @@
+package BubbleSort;
+
+import SelectionSort.V2.SelectionSort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 对数器的设计
+ * 1、产生2个数组，别忘了拷贝
+ * 2、用系统自带的排序算法和我们自己设计的排序算法进行排序
+ * 3、对比，两个数字每位上的数组是否一致
+ */
+public class DataCheck {
+
+    //随机数产生器
+    static int[] generateRandomArray(){
+        Random r = new Random();
+        int[] arr = new int[10000];
+        for (int i=0; i< arr.length; i++){
+            arr[i] = r.nextInt(10000);
+        }
+        return arr;
+    }
+
+    static void check(){
+        int[] arr = generateRandomArray();
+        int[] arr2 = new int[arr.length];
+        System.arraycopy(arr, 0, arr2,0, arr.length);
+
+        Arrays.sort(arr);
+        BubbleSort.sort(arr2);
+//        SelectionSort.sort(arr2);
+
+        boolean same = true;
+        for (int i=0; i< arr2.length; i++){
+            if (arr[i] != arr2[i])
+                same = false;
+        }
+        System.out.println(same == true ?"right" : "wrong");
+    }
+
+    public static void main(String[] args) {
+        check();
+    }
+}
